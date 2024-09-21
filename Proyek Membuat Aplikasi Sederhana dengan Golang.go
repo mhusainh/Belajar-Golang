0d158015cafaab1f19d9eee545305efcf2f0173a
mhusainh <husainhasbulah@gmail.com>
2024-09-21 23:51:57 +0700

    Menu Utama

---
+++ Proyek Membuat Aplikasi Sederhana dengan Golang.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Menampilkan menu utama beserta pilihannya
+	fmt.Println("======================================");
+	fmt.Println("\t      Menu Utama");
+	fmt.Println("======================================");
+	fmt.Println("1. Menampilkan Hello World!");
+	fmt.Println("2. Kalkulator Sederhana");
+	fmt.Println("3. Simpan dan Tampilkan Data Pengguna");
+	fmt.Println("4. Hitung Faktorial");
+	fmt.Println("5. Deret Fibonacci");
+	fmt.Println("6. Exit");
+	fmt.Println("======================================");
+	fmt.Print("Pilih Menu (1/2/3/4/5/6): ");
+}
